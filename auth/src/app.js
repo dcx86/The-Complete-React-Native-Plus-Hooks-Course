56358f6d3c1139c3afcecdd72499f964a37da2f5
Dragosh Cr <dragosh.craciun@gmail.com>
2019-09-09 18:45:02 +0200

    Firebase client setup

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import firebase from 'firebase'
+
 import { Header } from './components/common'
+import { apiKey, authDomain, databaseURL, storageBucket, messagingSenderId } from '../env'
 
 class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey,
+      authDomain,
+      databaseURL,
+      storageBucket,
+      messagingSenderId
+    })
+  }
+
   render() {
     return (
       <View>
