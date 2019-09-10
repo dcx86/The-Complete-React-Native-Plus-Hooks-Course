ffb58f9a72055307cc8edff53c9265582b62b1a3
Dragosh Cr <dragosh.craciun@gmail.com>
2019-09-10 07:19:14 +0200

    A focus on passing props

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import { TextInput } from 'react-native'
-
-import { Button, Card, CardSection } from './common'
+import { Button, Card, CardSection, Input } from './common'
 
 class LoginForm extends Component {
   state = { text: '' }
@@ -10,11 +8,7 @@ class LoginForm extends Component {
     return (
       <Card>
         <CardSection>
-          <TextInput
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
-            style={{ height: 220, width: 100 }}
-          />
+          <Input value={this.state.text} onChangeText={text => this.setState({ text })} />
         </CardSection>
         <CardSection />
         <CardSection>
--- auth/src/components/common/Input.js
+++ auth/src/components/common/Input.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { TextInput, View, Text } from 'react-native'
 
-const Input = ({ label }) => {
+const Input = ({ label, value, onChangeText }) => {
   return (
     <View>
       <Text>{label}</Text>
+      <TextInput value={value} onChangeText={onChangeText} style={{ height: 120, width: 100 }} />
     </View>
   )
 }
